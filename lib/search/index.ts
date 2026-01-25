e113880d8270a512b75aeb4f6fdc3ef35f467868
slaee <nasa.sly14@gmail.com>
2026-01-25 23:42:49 +0800

    Update notes list

--- lib/search/index.ts
+++ lib/search/index.ts
@@ -387,19 +387,35 @@ export const middleware: S.Middleware = (store) => {
         return next(withSearch(action));
       }
 
-      case 'CREATE_NOTE_WITH_ID':
-        if (action.note?.tags && action.note?.tags.length > 0) {
+      case 'CREATE_NOTE_WITH_ID': {
+        // Preserve the current collection context when creating a new note.
+        // The note is already assigned the correct folderId/tags by the data middleware.
+        const noteFolderId = action.note?.folderId as T.FolderId | null;
+        const noteTags = action.note?.tags;
+
+        if (noteFolderId) {
+          // If note was created in a folder, stay in that folder view
+          searchState.collection = {
+            type: 'folder',
+            folderId: noteFolderId,
+          };
+        } else if (noteTags && noteTags.length > 0) {
+          // If note has tags (created in a tag view), stay in that tag view
           searchState.collection = {
             type: 'tag',
-            tagName: action.note.tags[0],
+            tagName: noteTags[0],
           };
-        } else {
+        } else if (searchState.collection.type === 'trash') {
+          // Don't stay in trash when creating a note
           searchState.collection = { type: 'all' };
         }
+        // Otherwise, preserve the current collection (e.g., 'all', 'untagged')
+
         searchState.notes.set(action.noteId, toSearchNote(action.note ?? {}));
         indexNote(action.noteId);
         queueSearch();
         return next(action);
+      }
       case 'IMPORT_NOTE_WITH_ID':
       case 'REMOTE_NOTE_UPDATE':
       case 'RESTORE_NOTE_REVISION':
